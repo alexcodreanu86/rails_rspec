a2c80de7ec7712cac37a63d8beebef6ba3ad265f
thejessleigh <j.l.unrein@gmail.com>
2014-03-11 18:59:33 -0500

    all newly written tests pass. Should go back and 'refactor' the admin tests.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,30 +2,28 @@ require 'spec_helper'
 
 feature 'User browsing the website' do
   context "on homepage" do
-    let(:post) {Post.create(title: "test", content: "so testy", is_published: true) }
 
     it "sees a list of recent posts titles" do
+      Post.create(title: "test", content: "so testy", is_published: true)
       visit posts_url
-      expect {
-        response.body.should have_link("Test")
-      }
+      expect(page).to have_link("Test")
     end
 
     it "can click on titles of recent posts and should be on the post show page" do
-      pending
-      # given a user and a list of posts
-      # user visits the homepage
-      # when a user can clicks on a post title they should be on the post show page
+      Post.create(title: "test", content: "so testy", is_published: true)
+      visit posts_url
+      click_link("Test")
+      expect(page).to have_content("so testy")
     end
   end
 
   context "post show page" do
     it "sees title and body of the post" do
-      pending
-      # given a user and post(s)
-      # user visits the post show page
-      # user should see the post title
-      # user should see the post body
+      post = Post.create(title: "test", content: "so testy", is_published: true)
+
+      visit posts_url(post)
+      expect(page).to have_content("Test")
+      expect(page).to have_content("so testy")
     end
   end
 end
